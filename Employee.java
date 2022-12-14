fae9c118408270d4c3855a7ef5a266d1903b5476
novfeelgit <novfeelstar28@gmail.com>
2022-12-14 22:42:24 +0530

    Step111

---
+++ Employee.java
@@ -0,0 +1,26 @@
+package org.tcs;
+
+public class Employee {
+	private void add() {
+// datatype varable name = value
+		
+		String name = "Novfeel";
+		byte age = 127;
+		long phone = 9688999284l;
+		char gender= 'm';
+		
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(phone);
+		System.out.println(gender);
+		
+		
+		System.out.println("My name is : "+name);
+	
+	}
+	public static void main(String[] args) {
+		Employee a= new Employee();
+		a.add();
+	}
+
+}
